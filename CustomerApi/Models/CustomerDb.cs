af5f1b49aa1f9689ea0c01d9cd3b9080dbe84232
bilalkhan190 <90343366+bilalkhan190@users.noreply.github.com>
2022-03-15 16:52:15 +0500

    Add project files.

---
+++ CustomerApi/Models/CustomerDb.cs
@@ -0,0 +1,16 @@
+﻿using CustomerApi.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace CustomerApi.Modls
+{
+    public class CustomerDb:DbContext
+    {
+        public CustomerDb(DbContextOptions<CustomerDb> options) : base(options)
+        {
+
+        }
+
+        public DbSet<CustomerModel> Customers { get; set; }
+
+    }
+}
---
+++ CustomerApi/Models/CustomerModel.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CustomerApi.Models
+{
+    public class CustomerModel
+    {
+        [Key]
+        public int CustomerId { get; set; }
+        public string Customer { get; set; }
+        public string Address { get; set; }
+        public string Contact{ get; set; }
+        public string Email { get; set; }
+    }
+}
